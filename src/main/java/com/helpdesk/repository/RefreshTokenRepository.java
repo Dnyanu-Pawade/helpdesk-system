eeb44c5326af306e02f28aae44e3f939d705a555
Dnyaneshwar Pandurang Pawde <pawaded156@gmail.com>
2026-07-07 15:41:47 +0530

    Fix: @Modifying @Transactional on deleteByUser to fix no EntityManager error

--- src/main/java/com/helpdesk/repository/RefreshTokenRepository.java
+++ src/main/java/com/helpdesk/repository/RefreshTokenRepository.java
@@ -2,8 +2,14 @@ package com.helpdesk.repository;
 import com.helpdesk.entity.RefreshToken;
 import com.helpdesk.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 import java.util.Optional;
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByToken(String token);
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM RefreshToken r WHERE r.user = :user")
     void deleteByUser(User user);
 }
